638cbccebb184c07729d7b6d9f6c4cd5c0537c5f
Ceteris90 <jonas.nyador@mathmods.eu>
2023-04-26 17:20:20 -0700

    sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,13 +10,13 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i, j;
+	int temp;
+
 	if (array == NULL || size <= 1)
 	{
 		return;
 	}
-
-	size_t i, j;
-	int temp;
 
 	for (i = 0; i < size - 1; i++)
 	{
