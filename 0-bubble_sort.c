708c00a793095a9448b7ace258ace2e09275a417
Ceteris90 <jonas.nyador@mathmods.eu>
2023-04-26 17:10:27 -0700

    sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,6 +10,11 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+	if (array == NULL || size <= 1)
+	{
+		return;
+	}
+
 	size_t i, j;
 	int temp;
 
--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,8 +7,11 @@
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *prev, *temp;
+
 	if (*list == NULL || (*list)->next == NULL)
+	{
 		return;
+	}
 
 	current = (*list)->next;
 
